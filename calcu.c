2c8b241d182e3bab1e8b914c3517dcfb7a662b0e
thanuk.l <thanu.l@s.amity.edu>
2025-07-07 14:12:52 +0530

    multiplication

--- calcu.c
+++ calcu.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double num1, num2, sub;
+    double num1, num2, multi;
 
     printf("Enter two numbers to add: ");
     scanf("%lf %lf", &num1, &num2);
 
     sum = num1 + num2;
-    printf("The subtraction of %.2lf and %.2lf is %.2lf\n", num1, num2, sub);
+    printf("The multiplication of %.2lf and %.2lf is %.2lf\n", num1, num2, multi);
 
     return 0;
 }
