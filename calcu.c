9cad2f69abc1e69fb7ac776e28219bde6919220a
thanuk.l <thanu.l@s.amity.edu>
2025-07-07 14:11:40 +0530

    subtraction

--- calcu.c
+++ calcu.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double num1, num2, sum;
+    double num1, num2, sub;
 
     printf("Enter two numbers to add: ");
     scanf("%lf %lf", &num1, &num2);
 
     sum = num1 + num2;
-    printf("The sum of %.2lf and %.2lf is %.2lf\n", num1, num2, sum);
+    printf("The subtraction of %.2lf and %.2lf is %.2lf\n", num1, num2, sub);
 
     return 0;
 }
