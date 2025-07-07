44378d47e50af8cf92829dd7056ef92cbe65da2c
thanuk.l <thanu.l@s.amity.edu>
2025-07-07 14:13:45 +0530

    division

--- calcu.c
+++ calcu.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double num1, num2, multi;
+    double num1, num2, divi;
 
     printf("Enter two numbers to add: ");
     scanf("%lf %lf", &num1, &num2);
 
     sum = num1 + num2;
-    printf("The multiplication of %.2lf and %.2lf is %.2lf\n", num1, num2, multi);
+    printf("The division of %.2lf and %.2lf is %.2lf\n", num1, num2, divi);
 
     return 0;
 }
