bae824653c1ef0fcd305d4336b16b9c4557a20e8
thanu <thanu.l@s.amity.edu>
2025-07-07 14:17:32 +0530

    Update calcu.c

--- calcu.c
+++ calcu.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter two numbers to add: ");
     scanf("%lf %lf", &num1, &num2);
 
-    sum = num1 + num2;
+    sum = num1 / num2;
     printf("The division of %.2lf and %.2lf is %.2lf\n", num1, num2, divi);
 
     return 0;
